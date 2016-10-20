4037f898f6673de4228a2bdc6019c6c883dd111f
denghuiquan <980352129@qq.com>
2016-10-20 23:13:15 +0800

    fix: change from to from 2
    - `from` is a reserved and i hate `from from`

--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
 import crypto from 'crypto';
-import from from 'from2';
+import from2 from 'from2';
 
 // TODO publish to npm as `from2-buffer`
 // create a stream from a buffer
@@ -7,7 +7,7 @@ import from from 'from2';
 export function fromBuffer (buffer) {
   //assert.ok(Buffer.isBuffer(buffer))
 
-  return from(function (size, next) {
+  return from2(function (size, next) {
     if (buffer.length <= 0) {
       return this.push(null);
     }
